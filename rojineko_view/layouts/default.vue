6b505e15f86efae7091bf4a6db73cde3f15cbda7
ZYO-BAIKER <keisukeminakawa@gmail.com>
2022-02-14 11:28:57 +0900

    レイアウトを調整し、ヘッダーに必要な情報のみ残すように修正

--- rojineko_view/layouts/default.vue
+++ rojineko_view/layouts/default.vue
@@ -1,15 +1,6 @@
 <template>
   <v-app dark>
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
@@ -30,9 +21,6 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
-      clipped: false,
-      fixed: false,
-      miniVariant: false,
       title: 'Vuetify.js',
     }
   },
